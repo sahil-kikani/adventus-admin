5418894325989e3fecc81f436fb16df82e16064d
sahil-kikani <sahilkikani111@gmail.com>
2024-12-20 09:52:59 +0530

    change in app js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,66 +1,34 @@
-// ** Next Imports
 import Head from 'next/head'
 import { Router, useRouter } from 'next/router'
-
-// ** Store Imports
+import { CacheProvider } from '@emotion/react'
 import { store } from 'src/store'
 import { Provider } from 'react-redux'
-
-// ** Loader Import
 import NProgress from 'nprogress'
 
-// ** Emotion Imports
-import { CacheProvider } from '@emotion/react'
-
-// ** Config Imports
 import 'src/configs/i18n'
-import { defaultACLObj } from 'src/configs/acl'
+
 import themeConfig from 'src/configs/themeConfig'
-
-// ** Fake-DB Import
 import 'src/@fake-db'
-
-// ** Third Party Import
 import { Toaster } from 'react-hot-toast'
-
-// ** Component Imports
 import UserLayout from 'src/layouts/UserLayout'
-import AclGuard from 'src/@core/components/auth/AclGuard'
 import ThemeComponent from 'src/@core/theme/ThemeComponent'
-import AuthGuard from 'src/@core/components/auth/AuthGuard'
-import GuestGuard from 'src/@core/components/auth/GuestGuard'
-
-// ** Spinner Import
-import Spinner from 'src/@core/components/spinner'
-
-// ** Contexts
-import { AuthProvider } from 'src/context/AuthContext'
 import { SettingsConsumer, SettingsProvider } from 'src/@core/context/settingsContext'
-
-// ** Styled Components
 import ReactHotToast from 'src/@core/styles/libs/react-hot-toast'
-
-// ** Utils Imports
 import { createEmotionCache } from 'src/@core/utils/create-emotion-cache'
 
-// ** Prismjs Styles
 import 'prismjs'
 import 'prismjs/themes/prism-tomorrow.css'
 import 'prismjs/components/prism-jsx'
 import 'prismjs/components/prism-tsx'
-
-// ** React Perfect Scrollbar Style
 import 'react-perfect-scrollbar/dist/css/styles.css'
 import 'src/iconify-bundle/icons-bundle-react'
 
-// ** Global css styles
 import '../../styles/globals.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useEffect } from 'react'
 
 const clientSideEmotionCache = createEmotionCache()
 
-// ** Pace Loader
 if (themeConfig.routingLoader) {
   Router.events.on('routeChangeStart', () => {
     NProgress.start()
@@ -73,31 +41,15 @@ if (themeConfig.routingLoader) {
   })
 }
 
-const Guard = ({ children, authGuard, guestGuard }) => {
-  if (guestGuard) {
-    return <GuestGuard fallback={<Spinner />}>{children}</GuestGuard>
-  } else if (!guestGuard && !authGuard) {
-    return <>{children}</>
-  } else {
-    return <AuthGuard fallback={<Spinner />}>{children}</AuthGuard>
-  }
-}
-
-// ** Configure JSS & ClassName
 const App = props => {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
   const router = useRouter()
-  // Variables
+
   const contentHeightFixed = Component.contentHeightFixed ?? false
 
   const getLayout =
     Component.getLayout ?? (page => <UserLayout contentHeightFixed={contentHeightFixed}>{page}</UserLayout>)
   const setConfig = Component.setConfig ?? undefined
-  const authGuard = Component.authGuard ?? true
-  const guestGuard = Component.guestGuard ?? false
-  const aclAbilities = Component.acl ?? defaultACLObj
-
-  console.log('setConfig', setConfig)
 
   useEffect(() => {
     if (window !== 'undefined') {
@@ -130,18 +82,13 @@ const App = props => {
           <meta name='viewport' content='initial-scale=1, width=device-width' />
         </Head>
 
-        {/* <AuthProvider> */}
         <QueryClientProvider client={queryClient}>
           <SettingsProvider {...(setConfig ? { pageSettings: setConfig() } : {})}>
             <SettingsConsumer>
               {({ settings }) => {
                 return (
                   <ThemeComponent settings={settings}>
-                    {/* <Guard authGuard={authGuard} guestGuard={guestGuard}>
-                        <AclGuard aclAbilities={aclAbilities} guestGuard={guestGuard} authGuard={authGuard}> */}
                     {getLayout(<Component {...pageProps} />)}
-                    {/* </AclGuard> */}
-                    {/* </Guard> */}
                     <ReactHotToast>
                       <Toaster position={settings.toastPosition} toastOptions={{ className: 'react-hot-toast' }} />
                     </ReactHotToast>
@@ -151,7 +98,6 @@ const App = props => {
             </SettingsConsumer>
           </SettingsProvider>
         </QueryClientProvider>
-        {/* </AuthProvider> */}
       </CacheProvider>
     </Provider>
   )
